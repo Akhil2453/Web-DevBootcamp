be5228d7dafb15e3738601b547e1ea9a82e4a97a
Akhil2453 <akhil.menon94@gmail.com>
2018-03-20 10:52:30 +0530

    exercis js -- game

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,10 @@
-var uName = prompt("What is your name?");
-alert("Nice to meet you "+ uName);
-console.log("Also great to meet you "+uName);
+// var uName = prompt("What is your name?");
+// alert("Nice to meet you "+ uName);
+// console.log("Also great to meet you "+uName);
+
+var uName = prompt("What is your name??");
+var lName = prompt("What is your last name?");
+var age = prompt("What is you age??");
+
+console.log("Your full name is " + uName + " " + lName);
+console.log("You are " + age + " years old");
